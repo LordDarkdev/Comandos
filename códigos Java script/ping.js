ffddb509067ea4119a96b05c8389b94af2e3fcff
Lord <148513662+LordDarkdev@users.noreply.github.com>
2023-11-25 17:30:51 -0300

    Update ping.js

--- códigos Java script/ping.js
+++ códigos Java script/ping.js
@@ -1,1 +1,16 @@
 
+
+const Discord = require('discord.js')
+
+Module.exports = {
+name: "Ping",
+run: async (client, message) = {
+let embedping = new Discord.EmbedBuilder()
+.setColor("ff0000")
+.setDescription(`${message.author} Pong! ${client.ws.ping}ms`)
+
+message.reply({embeds: [embedping]})
+}
+}
+
+
