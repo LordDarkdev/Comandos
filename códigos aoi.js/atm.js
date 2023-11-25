9f0b16756f2a857abbae03c9a95186dd8157b8e8
Lord <148513662+LordDarkdev@users.noreply.github.com>
2023-11-25 16:47:55 -0300

    Update atm.js

--- códigos aoi.js/atm.js
+++ códigos aoi.js/atm.js
@@ -1,8 +1,13 @@
 module.exports = [{
 name: "atm",
 aliases: ['bal', 'money', 'dinheiro', 'saldo'],
+$if: "old",
 code: `
+$if[$mentioned[1]==]
 💸 | <@$authorID> Você tem **$numberSeparator[$getGlobalUserVar[$get[money];$authorID]] Coins **, atualmente você está na posição **#$getLeaderboardinfo[$get[money];$authorID;globalUser;top]] ** Do rank.
+$else
+💸 | <@$mentioned[1]> Tem **$numberSeparator[$getGlobalUserVar[$get[money];$mentioned[1]]] Coins **, atualmente está na posição **#$getLeaderboardinfo[$get[money];$mentioned[1];globalUser;top]] ** Do rank.
+
 $let[money;SUA VAR DE DINHEIRO]
 `
 }] 
